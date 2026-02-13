ddb36f6e46e84675a9866651be0817d52009ef94
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-02-13 18:15:23 +0000

    Feb 13, 2026, 11:45 PM

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use("/api/v1/tours", noteRouter);
+app.use("/api/v1/notes", noteRouter);
 // app.use("/api/v1/users", userRouter);
 
 module.exports = app;
