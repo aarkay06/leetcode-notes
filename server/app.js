f4d363db6168e2516a52c58ba7d79b2c71bdae43
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-02-14 07:38:31 +0000

    Feb 14, 2026, 1:08 PM

--- server/app.js
+++ server/app.js
@@ -1,31 +1,15 @@
 const express = require("express");
-
-// const tourRouter = require('./routes/tourRoutes');
-// const userRouter = require('./routes/userRoutes');
-
 const noteRouter = require("./routes/noteRoutes");
 
 const app = express();
 
-// 1) MIDDLEWARES
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware 👋");
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
-// 3) ROUTES
 app.use("/api/v1/notes", noteRouter);
 // app.use("/api/v1/users", userRouter);
 
