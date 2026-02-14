15177ca0d4eb555a890325f96145d8117de94e64
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-02-14 07:22:08 +0000

    Feb 14, 2026, 12:52 PM

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -33,7 +33,22 @@ exports.createNote = catchAsync(async (req, res) => {
   });
 });
 
-exports.updateNote = catchAsync(async (req, res) => {});
+exports.updateNotes = catchAsync(async (req, res, next) => {
+  const note = await Note.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!note) {
+    return next(new AppError("No note found with that id", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      note,
+    },
+  });
+});
 
 exports.deleteNote = catchAsync(async (req, res) => {
   await Note.deleteMany();
