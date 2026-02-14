aa48e95c4c7a6a31bfcaf6e86e1e895a9ae60cff
KrishFX <88158135+aarkay06@users.noreply.github.com>
2026-02-14 13:42:42 +0000

    Feb 14, 2026, 7:12 PM

--- server/daemon.js
+++ server/daemon.js
@@ -36,9 +36,9 @@ app.post("/api/problems", async (req, res) => {
 
   try {
     console.log(data);
-    // const filePath = saveToLocalFile(data);
-    // // We don't await this because we want to reply to Chrome fast
-    // pushToCloud(data).catch((err) => alert("Cloud push failed:", err.message));
+    const filePath = saveToLocalFile(data);
+    // We don't await this because we want to reply to Chrome fast
+    pushToCloud(data).catch((err) => alert("Cloud push failed:", err.message));
 
     res.json({ success: true, filename: path.basename(filePath) });
   } catch (err) {
